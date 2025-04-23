c84a92d9d955c910222038274d444b47a0f456c9
luizfelipew <luizfelipewendt@gmail.com>
2025-04-23 16:30:08 -0300

    Começo de projeto

--- api-ai/src/test/java/com/luiz/api_ai/ApiAiApplicationTests.java
+++ api-ai/src/test/java/com/luiz/api_ai/ApiAiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.luiz.api_ai;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiAiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.luiz.api_ai;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApiAiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
